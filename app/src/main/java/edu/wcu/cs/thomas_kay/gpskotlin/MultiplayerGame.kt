d286d8d6d1352965a45e45a8b9e4a36f2a3965e5
Thomas Kay <takay1@catamount.wcu.edu>
2024-05-10 10:48:05 -0400

    Quick documentation to unfinished multiplayer data structure

--- app/src/main/java/edu/wcu/cs/thomas_kay/gpskotlin/MultiplayerGame.kt
+++ app/src/main/java/edu/wcu/cs/thomas_kay/gpskotlin/MultiplayerGame.kt
@@ -7,17 +7,31 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
+
+/**
+ * Not implemented yet
+ */
+
+/** Reference to the multiplayer game database to host games  */
 const val MULTIPLAYER_NAME = "Games"
+/** The required length for the code/id of a game */
 const val CODE_LENGTH = 6
+/** Half */
 const val HALF = .5
 
 class MultiplayerGame(var key:String? = null) {
+    /** Code/id of the game to join/host */
     lateinit var code:String
+    /** List of each player's coordinates */
     lateinit var playerCoords: ArrayList<LatLng>
+    /** List of each player's time */
     lateinit var playerTime: ArrayList<Int>
+    /** List of each player's qr codes */
     lateinit var playerQRCodes: ArrayList<Int>
+    /** Reference to multiplayer game database */
     val reference = FirebaseDatabase.getInstance().getReference(MULTIPLAYER_NAME)
 
+    /** Sets up reference to server */
     init {
         reference.addValueEventListener(object:ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -46,6 +60,7 @@ class MultiplayerGame(var key:String? = null) {
 
 
 
+    /** Create a new game entry */
     fun createGame() {
         generateCode()
         val game = Game(this.code)
@@ -53,12 +68,14 @@ class MultiplayerGame(var key:String? = null) {
         reference.child(key!!).setValue(game)
     }
 
+    /** Join a game from an inputted code */
     fun joinGame(code:String) {
         val player = Player()
         val playerKey = reference.child(key!!).push().key
         reference.child(key!!).child(playerKey!!).setValue(player)
     }
 
+    /** Generate the code to be used for a multiplayer game */
     private fun generateCode() {
         var code = ""
         for(i in 0..<CODE_LENGTH) {
@@ -74,6 +91,7 @@ class MultiplayerGame(var key:String? = null) {
         this.code = code
     }
 
+    /** Remove the game from the database */
     fun destoryGame() {
         this.reference.child(key!!).removeValue()
     }
@@ -82,6 +100,7 @@ class MultiplayerGame(var key:String? = null) {
 
 }
 
+//Not used data classes
 data class Game(val code:String = "")
 
 data class Player(val lat: Double = 0.0, val lng: Double = 0.0, val time: Int = 0,
